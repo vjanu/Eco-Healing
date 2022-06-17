f9fa105961960b3a62479d373d7b3cb4347897b1
dhruvilshah1999 <68918132+dhruvilshah1999@users.noreply.github.com>
2022-06-17 18:39:07 -0400

    Basic UI for Add Food Item with Home Navigations
    -Added HomeScreen with 3 Add Child Buttons.
    -Added Basic UI of Food Item page.
    -Dependencies added

--- lib/mainScreen/HomeScreen.dart
+++ lib/mainScreen/HomeScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:eco_healing/add_items/add_food_items.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -7,9 +10,66 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  ValueNotifier<bool> isDialOpen = ValueNotifier(false);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+
+      onWillPop: () async{
+        if(isDialOpen.value){
+          isDialOpen.value = false;
+          return false;
+        }else{
+          return true;
+        }
+      },
+
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Welcome"),
+          backgroundColor: Colors.lightGreen,
+        ),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.add_event,
+          backgroundColor: Colors.deepOrangeAccent,
+          overlayColor: Colors.lightGreen,
+          openCloseDial: isDialOpen,
+          overlayOpacity: 0,
+          spacing: 10,
+          spaceBetweenChildren: 10,
+          children: [
+            SpeedDialChild(
+                child: Icon(Icons.fastfood_rounded),
+                label: 'Upload Food Items',
+                backgroundColor: Colors.lightGreen,
+                onTap: (){
+                  print('Food Button Selected');
+                  Navigator.push(context, MaterialPageRoute(builder: (c)=>const AddFood_itmes()));
+                }
+            ),
+            SpeedDialChild(
+                child: Icon(Icons.local_shipping_rounded),
+                label: 'Upload Cloth Items',
+                backgroundColor: Colors.lightGreen,
+                onTap: (){
+                  print('Cloth Button Selected');
+                }
+            ),
+            SpeedDialChild(
+                child: Icon(Icons.electrical_services_rounded),
+                label: 'Upload Eletronic Items',
+                backgroundColor: Colors.lightGreen,
+                onTap: (){
+                  print('Electoric  Selected');
+                }
+            )
+          ],
+
+        ),
+      ),
+    );
   }
 }
 
