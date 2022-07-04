23f4a8a6712f5eb4087c0d7005c04db91fdaece5
dhruvilshah1999 <68918132+dhruvilshah1999@users.noreply.github.com>
2022-07-04 19:08:09 -0400

    Error Fix
    Henish's Code retrived

--- lib/auth/auth_screen.dart
+++ lib/auth/auth_screen.dart
@@ -16,43 +16,43 @@ class _AuthScreenState extends State<AuthScreen> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-          appBar: AppBar(
-              flexibleSpace: Container(
-                  decoration: const BoxDecoration(
+        appBar: AppBar(
+            flexibleSpace: Container(
+                decoration: const BoxDecoration(
                     gradient:LinearGradient(
-                        colors:[
-                          Colors.green,
-                          Colors.lightGreen,
-                         // Colors.lightGreenAccent
-                        ],
-                       // begin:FractionalOffset(0.0,0.0),
-                    //end:FractionalOffset(1.0,0.0),
-                    //stops:[0.0, 1.0],
-                   // tileMode: TileMode.clamp,
-                  )
-              )
-          ),
-          automaticallyImplyLeading: false,
-          title:const Text('Eco-Healing'),
-          centerTitle:true,
-          bottom:const TabBar(
-              tabs:[
-                Tab(
-
-                  icon:Icon(Icons.person,color:Colors.white),
-                  text: "Sign Up",
-                ),
-                Tab(
-                  icon:Icon(Icons.lock,color:Colors.white),
-                  text: "Login",
+                      colors:[
+                        Colors.green,
+                        Colors.lightGreen,
+                        // Colors.lightGreenAccent
+                      ],
+                      // begin:FractionalOffset(0.0,0.0),
+                      //end:FractionalOffset(1.0,0.0),
+                      //stops:[0.0, 1.0],
+                      // tileMode: TileMode.clamp,
+                    )
                 )
+            ),
+            automaticallyImplyLeading: false,
+            title:const Text('Eco-Healing'),
+            centerTitle:true,
+            bottom:const TabBar(
+                tabs:[
+                  Tab(
 
-              ]
-          )
+                    icon:Icon(Icons.person,color:Colors.white),
+                    text: "Sign Up",
+                  ),
+                  Tab(
+                    icon:Icon(Icons.lock,color:Colors.white),
+                    text: "Login",
+                  )
 
-      ),
-          body: Container(
-              decoration: const BoxDecoration(
+                ]
+            )
+
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
               gradient:LinearGradient(
                 colors:[
                   Colors.green,
@@ -65,18 +65,17 @@ class _AuthScreenState extends State<AuthScreen> {
                 // tileMode: TileMode.clamp,
               )
           ),
-              child:const TabBarView(
-              children: [
-                SignupScreen(),
-                login_page(),
+          child:const TabBarView(
+            children: [
+              SignupScreen(),
+              login_page(),
 
 
             ],
           ),
-          ),
+        ),
 
-          ),
+      ),
     );
   }
 }
-
--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,6 +1,12 @@
+import 'package:eco_healing/Widget/error_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eco_healing/Widget/loading_dialog.dart';
+import 'package:eco_healing/mainScreen/HomeScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:eco_healing/Widget/custom_field.dart';
-//import 'package:flutter/services.dart';
+import 'package:eco_healing/global/global.dart';
+// import 'package:flutter/services.dart';
 
 class login_page extends StatefulWidget {
   const login_page({Key? key}) : super(key: key);
@@ -14,11 +20,79 @@ class _login_pageState extends State<login_page> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  formValidation() {
+    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+      //login
+      loginNow();
+    } else {
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(
+              message: "Please write email/password.",
+            );
+          });
+    }
+  }
+
+  loginNow() async {
+    showDialog(
+        context: context,
+        builder: (c) {
+          return LoadDialog(
+            message: "Checking Credentials",
+          );
+        });
+
+    User? currentUser;
+    await firebaseAuth
+        .signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    )
+        .then((auth) {
+      currentUser = auth.user!;
+    }).catchError((error) {
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(
+              message: error.message.toString(),
+            );
+          });
+    });
+    if (currentUser != null) {
+      // readDataAndSetDataLocally(currentUser!).then((value) {
+      // ignore: use_build_context_synchronously
+      Navigator.pop(context);
+      // ignore: use_build_context_synchronously
+      Navigator.push(
+          context, MaterialPageRoute(builder: (c) => const HomeScreen()));
+      // });
+    }
+  }
+
+  // Future readDataAndSetDataLocally(User currentUser) async {
+  //   await FirebaseFirestore.instance
+  //       .collection("Users")
+  //       .doc(currentUser.uid)
+  //       .get()
+  //       .then((snapshot) async {
+  //     await sharedPreferences!.setString("userID", currentUser.uid);
+  //     await sharedPreferences!
+  //         .setString("email", snapshot.data()!["userEmail"]);
+  //     await sharedPreferences!
+  //         .setString("name", snapshot.data()!["userName"]);
+  //     await sharedPreferences!
+  //         .setString("photoUrl", snapshot.data()!["userAvatarUrl"]);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child:Column(
+      child: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
           Container(
@@ -26,8 +100,7 @@ class _login_pageState extends State<login_page> {
             child: Padding(
               padding: EdgeInsets.all(15),
               child: Image.asset(
-
-                "images/earth-day.png", //"images/loginpage.png",
+                "images/ecohealing.jpeg", //"images/loginpage.png",
                 height: 270,
               ),
             ),
@@ -35,40 +108,41 @@ class _login_pageState extends State<login_page> {
           Form(
               key: _formkey,
               child: Column(
-              children:[
-               Customfield(
-                 data: Icons.email,
-                 controller: emailController,
-                 hintText: "Email",
-                 isObsecure:false,
-               ),
-                Customfield(
-                  data: Icons.lock,
-                  controller: passwordController,
-                  hintText: "Password",
-                  isObsecure:true,
-                ),
-              ],
-              )
-          ),
+                children: [
+                  Customfield(
+                    data: Icons.email,
+                    controller: emailController,
+                    hintText: "Email",
+                    isObsecure: false,
+                  ),
+                  Customfield(
+                    data: Icons.lock,
+                    controller: passwordController,
+                    hintText: "Password",
+                    isObsecure: true,
+                  ),
+                ],
+              )),
           ElevatedButton(
-            child: const Text(
-              "Login",
-              style:TextStyle(
-                color:Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
             style: ElevatedButton.styleFrom(
               primary: Colors.cyan,
               padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
             ),
-
-            onPressed: ()=> print("Clicked"),
+            onPressed: () {
+              formValidation();
+            },
+            child: const Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-          const SizedBox(height: 30,),
+          const SizedBox(
+            height: 30,
+          ),
         ],
-
       ),
     );
   }
